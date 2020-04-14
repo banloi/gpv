2be3ce3cb1ea85fe0121c58353091e3b3796434f
Banloi.com <clear1900@protonmail.com>
2020-04-14 21:33:18 +0800

    add adm login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import qs from 'qs'
 import config from '../config'
-import histroy from '../history'
+import history from '../history'
 
 class Login extends Component {
   constructor () {
     super()
     this.state = {
-      number: '',
+      accout: '',
       password: '',
+      type: 'user',
       message: '请登录'
     }
     this.handleInput = this.handleInput.bind(this)
@@ -30,16 +31,29 @@ class Login extends Component {
 
   // 登录操作
   handleLogin () {
-    const { number, password } = this.state
-    axios.post(`${config.baseURL}/user/login`, qs.stringify(
-      {
-        number: number,
+    const { accout, password, type } = this.state
+    let data
+    if (type === 'user') {
+      data = {
+        number: accout,
         password: password
       }
-    ))
+    } else if (type === 'adm') {
+      data = {
+        name: accout,
+        password: password
+      }
+    }
+    axios.post(`${config.baseURL}/${type}/login`, qs.stringify(data))
       .then(res => {
         if (res.status === 200) {
-          histroy.push('/u/activities')
+          if (res.data.type === 'user') {
+            history.push('/u/activities')
+          } else if (res.data.type === 'actAdm') {
+            history.push('/a/act/new')
+          } else if (res.data.type === 'userAdm') {
+            history.push('/a/user')
+          }
         } else if (res.status === 400) {
           console.log(res.json)
         }
@@ -54,7 +68,7 @@ class Login extends Component {
   }
 
   render () {
-    const { number, password, message } = this.state
+    const { accout, password, message } = this.state
     return (
       <div className='login'>
         {message}
@@ -62,8 +76,8 @@ class Login extends Component {
           <div>
             <label>账号</label>
             <input
-              value={number}
-              name='number'
+              value={accout}
+              name='accout'
               onChange={this.handleInput}
               placeholder='输入账号'
               type='text'
@@ -80,6 +94,12 @@ class Login extends Component {
               type='password'
               required
             />
+          </div>
+          <div>
+            <input type='radio' value='user' name='type' id='user' checked onChange={this.handleInput} />
+            <label>用户</label>
+            <input type='radio' value='adm' name='type' id='adm' onChange={this.handleInput} />
+            <label>管理员</label>
           </div>
           <input type='submit' value='登录' onClick={this.handleLogin} />
         </form>
