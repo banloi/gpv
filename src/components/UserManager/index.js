a1ec1c2f7a201215e2e6535675fbe0bcfa183fae
Banloi.com <clear1900@protonmail.com>
2020-04-14 21:35:06 +0800

    add user adm page

---
+++ src/components/UserManager/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './index.css'
+// import axios from 'axios'
+// import history from '../history'
+// import { Route, Router, Link, Switch } from 'react-router-dom'
+// import config from '../config'
+// import qs from 'qs'
+
+class UserManager extends Component {
+  render () {
+    return (
+      <div>
+        用户管理员界面
+      </div>
+    )
+  }
+}
+
+export default UserManager
